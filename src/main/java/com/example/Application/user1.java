50423fc0d378e13ba05ec7ce1089382417b5fb45
jawidmah <jawidmah@UTORONTO.CA>
2019-11-22 21:02:44 +0000

    MJ: added starter code of phase 3 of the project

---
+++ src/main/java/com/example/Application/user1.java
@@ -0,0 +1,14 @@
+package com.example.Application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class user1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user1);
+    }
+}
