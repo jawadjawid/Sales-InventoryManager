ad3dff550966e1a38e0efc56e1a67eb8d9e913ec
dahalksh <dahalksh@UTORONTO.CA>
2019-11-29 13:48:21 +0000

    KD: Worked on Initial Admin Sign Up page such that if the user is not running the app for the first time, it is redirected instead to the login page.

--- src/main/java/com/Application/Controller/InitialAdminSignupController.java
+++ src/main/java/com/Application/Controller/InitialAdminSignupController.java
@@ -9,6 +9,7 @@ import com.Application.Model.database.helper.DatabaseDriverAndroidHelper;
 import com.Application.Model.exceptions.DatabaseInsertException;
 import com.Application.View.InitialAdminSignupView;
 import com.Application.View.InitialEmployeeSignupView;
+import com.Application.View.MainLoginView;
 
 import java.math.BigDecimal;
 
@@ -19,45 +20,62 @@ public class InitialAdminSignupController extends SignupController implements Vi
     public InitialAdminSignupController(Context context){
         appContext = context;
         view = (InitialAdminSignupView) appContext;
+
+        DatabaseDriverAndroidHelper mydb = new DatabaseDriverAndroidHelper(view);
+
+        // Uncomment below to go to admin sign up page
+        // appContext.deleteDatabase("inventorymgmt.db");
+
+
+        if(mydb.getUserDetailsH(2) == null){
+            // first run
+            dbSetup(mydb);
+        }else{
+            Intent intent = new Intent(appContext, MainLoginView.class);
+            appContext.startActivity(intent);
+        }
     }
 
     @Override
     public void onClick(View v) {
         try {
-            // admin has clicked sign up
             DatabaseDriverAndroidHelper mydb = new DatabaseDriverAndroidHelper(view);
-            dbSetup(mydb);
+
             int userId = createUser(view, mydb,1);
 
             Toast.makeText(view,"User with id " + userId + " created.",Toast.LENGTH_SHORT).show();
 
             Intent intent = new Intent(appContext, InitialEmployeeSignupView.class);
             appContext.startActivity(intent);
+
         }catch(DatabaseInsertException | NumberFormatException e){
             Toast.makeText(view,"Please ensure that all fields are completed.",Toast.LENGTH_SHORT).show();
             e.printStackTrace();
         }
     }
 
-    private void dbSetup(DatabaseDriverAndroidHelper mydb) throws DatabaseInsertException {
-        appContext.deleteDatabase("inventorymgmt.db");
+    private void dbSetup(DatabaseDriverAndroidHelper mydb) {
+        try {
+            appContext.deleteDatabase("inventorymgmt.db");
 
-        mydb.insertRoleH("ADMIN");
-        mydb.insertRoleH("EMPLOYEE");
-        mydb.insertRoleH("CUSTOMER");
+            mydb.insertRoleH("ADMIN");
+            mydb.insertRoleH("EMPLOYEE");
+            mydb.insertRoleH("CUSTOMER");
 
-        mydb.insertItemH("FISHING_ROD", new BigDecimal("12.00").setScale(2));
-        mydb.insertItemH("HOCKEY_STICK", new BigDecimal("10.00").setScale(2));
-        mydb.insertItemH("SKATES", new BigDecimal("9.00").setScale(2));
-        mydb.insertItemH("RUNNING_SHOES", new BigDecimal("15.00").setScale(2));
-        mydb.insertItemH("PROTEIN_BAR", new BigDecimal("5.00").setScale(2));
+            mydb.insertItemH("FISHING_ROD", new BigDecimal("12.00").setScale(2));
+            mydb.insertItemH("HOCKEY_STICK", new BigDecimal("10.00").setScale(2));
+            mydb.insertItemH("SKATES", new BigDecimal("9.00").setScale(2));
+            mydb.insertItemH("RUNNING_SHOES", new BigDecimal("15.00").setScale(2));
+            mydb.insertItemH("PROTEIN_BAR", new BigDecimal("5.00").setScale(2));
 
-        mydb.insertInventoryH(1,0);
-        mydb.insertInventoryH(2,0);
-        mydb.insertInventoryH(3,0);
-        mydb.insertInventoryH(4,0);
-        mydb.insertInventoryH(5,0);
+            mydb.insertInventoryH(1, 0);
+            mydb.insertInventoryH(2, 0);
+            mydb.insertInventoryH(3, 0);
+            mydb.insertInventoryH(4, 0);
+            mydb.insertInventoryH(5, 0);
+        }catch (DatabaseInsertException e){
 
+        }
     }
 
 
